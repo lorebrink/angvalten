6e1886da9ac35b3acbfee261c493cdfa35a6b3e2
Lukas Lorentzon <lorentzon.lukas@outlook.com>
2020-01-18 21:51:48 +0100

    Building project! Many warning though ;)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
+#include <sc2api/sc2_api.h>
+
 #include <iostream>
 
-int main() {
+using namespace sc2;
 
-	std::cout << "Kom igen!" << std::endl;
+class Bot : public Agent {
+public:
+
+	virtual void OnStep() final {
+		std::cout << Observation()->GetGameLoop() << std::endl;
+	}
+
+	virtual void OnUnitIdle(const Unit* unit) final {
+		switch (unit->unit_type.ToType()) {
+		case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
+			Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
+			break;
+		}
+		default: {
+			break;
+		}
+		}
+	}
+};
+
+int main(int argc, char* argv[]) {
+
+	Coordinator coordinator;
+	coordinator.LoadSettings(argc, argv);
+
+	Bot bot;
+	coordinator.SetParticipants({
+		CreateParticipant(Race::Terran, &bot),
+		CreateComputer(Race::Zerg)
+		});
+
+	coordinator.LaunchStarcraft();
+	coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+
+	while (coordinator.Update()) {}
 
 	return 0;
 }
