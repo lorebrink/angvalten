c50fd1c47173d8a5342db7dfbb31f902d8d53481
Serebrink <johan.serebrink@gmail.com>
2020-01-18 22:28:39 +0100

    Changed the default team to zerg

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
 
 	Bot bot;
 	coordinator.SetParticipants({
-		CreateParticipant(Race::Terran, &bot),
+		CreateParticipant(Race::Zerg, &bot),
 		CreateComputer(Race::Zerg)
 		});
 
